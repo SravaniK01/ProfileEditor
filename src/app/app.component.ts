56d6a0cbea582ae756ed2b7caecbe0180508bb3d
SravaniKadari <ksravredi@gmail.com>
2022-08-29 15:46:03 -0700

    profileEditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './interfaces/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'profileEditor';
+  user: User = { name:' Raja',age:30, favoriteColor:'blue'}
+  title:string = 'User Information'
+  
+  formUser: User = {
+    name:' Raja',age:30, favoriteColor:'blue'
+  }
+
+  displayEdit:boolean = false;
+
+  toggleEdit() :void{
+    this.displayEdit = !this.displayEdit;
+    if(this.displayEdit)
+    this.title = 'Updated User Information'
+    else
+    this.title = 'User Information'
+  }
+
+  saveChanges(): void {
+    this.user.name = this.formUser.name;
+    this.user.age = this.formUser.age;
+    this.user.favoriteColor = this.formUser.favoriteColor;
+
+  }
 }
